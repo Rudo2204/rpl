a33893426ff1f604241c790c87001d38d56ab8c5
Rudo2204 <rudo2204@gmail.com>
2021-05-28 23:58:29 +0700

    Move util to librpl

--- src/librpl/util.rs
+++ src/librpl/util.rs
@@ -1,15 +1,18 @@
-use anyhow::{anyhow, Result};
 use directories::ProjectDirs;
-use log::{debug, error, info};
+use log::debug;
 use std::path::{Path, PathBuf};
 
-pub fn get_data_dir(qualifier: &str, organization: &str, application: &str) -> Result<PathBuf> {
+pub fn get_data_dir(
+    qualifier: &str,
+    organization: &str,
+    application: &str,
+) -> anyhow::Result<PathBuf> {
     let proj_dirs = ProjectDirs::from(&qualifier, &organization, &application)
         .expect("Could not retrieve ProjectDirs, maybe you are using an unsupported OS");
     Ok(proj_dirs.data_dir().to_path_buf())
 }
 
-pub fn create_data_dir(data_dir: &Path) -> Result<()> {
+pub fn create_data_dir(data_dir: &Path) -> anyhow::Result<()> {
     if !data_dir.exists() {
         debug!("Project data dir does not exist, creating them...");
         std::fs::create_dir_all(data_dir)?;
