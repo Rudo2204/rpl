21cc84efc4e2d7234b6d1b40bedd0185c725f85f
Rudo2204 <rudo2204@gmail.com>
2021-05-29 22:09:11 +0700

    Initial custom errors

---
+++ src/librpl/error.rs
@@ -0,0 +1,21 @@
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Request Error when talking to qbittorrent: {0}")]
+    ReqErr(#[from] reqwest::Error),
+    #[error("Could not convert reqwest header to string: {0}")]
+    ToStringError(#[from] reqwest::header::ToStrError),
+    #[error("Serde json could not correctly deserialize: {0}")]
+    SerdeJson(#[from] serde_json::Error),
+    #[error("Serde_urlencoded could not serialize the url: {0}")]
+    SerdeUrl(#[from] serde_urlencoded::ser::Error),
+    #[error("Header value was malformed: {0}")]
+    HeaderError(#[from] reqwest::header::InvalidHeaderValue),
+    #[error("Header value was not correctly set")]
+    MissingHeaders,
+    #[error("Cookie value was not correctly set")]
+    MissingCookie,
+    #[error("Could not slice off SID from returned cookie")]
+    SliceError,
+    #[error("The torrent added does not have any files")]
+    EmptyTorrent,
+}
