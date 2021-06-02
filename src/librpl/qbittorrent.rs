e7a771658385523918e08f099a58962e91f94fd1
Rudo2204 <rudo2204@gmail.com>
2021-06-02 18:21:08 +0700

    Initial support to add torrent to client

--- src/librpl/qbittorrent.rs
+++ src/librpl/qbittorrent.rs
@@ -2,6 +2,38 @@
 #![allow(unused_imports)]
 
 use crate::librpl::error;
+use derive_builder::Builder;
+use lava_torrent::torrent::v1::Torrent;
+use reqwest::multipart::{Form, Part};
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+#[derive(Debug, Clone, Deserialize, Serialize, Builder, Default)]
+#[builder(setter(into, strip_option))]
+pub struct TorrentDownload {
+    #[builder(default)]
+    urls: Option<String>,
+    #[builder(default)]
+    torrents: Option<Vec<u8>>,
+    #[builder(default)]
+    savepath: Option<String>,
+    #[builder(default)]
+    cookie: Option<String>,
+    #[builder(default)]
+    skip_checking: Option<String>,
+    #[builder(default)]
+    paused: Option<String>,
+    #[builder(default)]
+    root_folder: Option<String>,
+    #[builder(default)]
+    rename: Option<String>,
+    #[builder(default)]
+    #[serde(rename = "upLimit")]
+    upload_limit: Option<i64>,
+    #[builder(default)]
+    #[serde(rename = "dlLimit")]
+    download_limit: Option<i64>,
+}
 
 pub struct QbitConfig {
     cookie: String,
@@ -58,5 +90,114 @@ impl QbitConfig {
             .text()
             .await?;
         Ok(res)
+    }
+
+    fn make_headers(&self) -> Result<reqwest::header::HeaderMap, error::Error> {
+        let mut headers = reqwest::header::HeaderMap::new();
+        headers.insert("cookie", self.cookie.parse()?);
+        Ok(headers)
+    }
+
+    async fn add_new_torrent(&self, data: TorrentDownload) -> Result<(), error::Error> {
+        let res = self
+            .client
+            .post(&format!("{}/api/v2/torrents/add", self.address))
+            .multipart(data.build_form())
+            .headers(self.make_headers()?)
+            .send()
+            .await?;
+
+        match res.error_for_status() {
+            Ok(_) => Ok(()),
+            Err(e) => Err(error::Error::from(e)),
+        }
+    }
+}
+
+impl TorrentDownload {
+    fn build_form(self) -> Form {
+        let mut form = Form::new();
+        form = match self.urls {
+            Some(urls) => form.text("urls", urls),
+            None => form,
+        };
+        form = match self.torrents {
+            Some(torrents) => form.part("urls", Part::bytes(torrents).file_name("")),
+            None => form,
+        };
+        form = match self.savepath {
+            Some(savepath) => form.text("savepath", savepath),
+            None => form,
+        };
+        form = match self.cookie {
+            Some(cookie) => form.text("cookie", cookie),
+            None => form,
+        };
+        form = match self.skip_checking {
+            Some(skip_checking) => form.text("skip_checking", skip_checking),
+            None => form,
+        };
+        form = match self.paused {
+            Some(paused) => form.text("paused", paused),
+            None => form,
+        };
+        form = match self.root_folder {
+            Some(root_folder) => form.text("root_folder", root_folder),
+            None => form,
+        };
+        form = match self.rename {
+            Some(rename) => form.text("rename", rename),
+            None => form,
+        };
+        form = match self.upload_limit {
+            Some(upload_limit) => form.text("upLimit", upload_limit.to_string()),
+            None => form,
+        };
+        form = match self.download_limit {
+            Some(download_limit) => form.text("dlLimit", download_limit.to_string()),
+            None => form,
+        };
+        form
+    }
+
+    pub async fn download(self, qbit: &QbitConfig) -> Result<(), error::Error> {
+        qbit.add_new_torrent(self).await
+    }
+
+    pub fn url(mut self, url: String) -> Self {
+        self.urls = Some(url);
+        self
+    }
+
+    pub fn torrents(mut self, torrent: Torrent) -> Self {
+        self.torrents = Some(
+            torrent
+                .encode()
+                .expect("Could not encode Torrent to bencode Vec u8"),
+        );
+        self
+    }
+
+    pub fn save_path(mut self, path: PathBuf) -> Self {
+        self.savepath = Some(String::from(
+            path.to_str().expect("Could not convert save path PathBuf"),
+        ));
+        self
+    }
+
+    pub fn skip_hash_checking(mut self, skip: bool) -> Self {
+        self.skip_checking = match skip {
+            true => Some(String::from("true")),
+            false => Some(String::from("false")),
+        };
+        self
+    }
+
+    pub fn paused(mut self, paused: bool) -> Self {
+        self.paused = match paused {
+            true => Some(String::from("true")),
+            false => Some(String::from("false")),
+        };
+        self
     }
 }
