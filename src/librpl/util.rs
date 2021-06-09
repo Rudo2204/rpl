74b3c3d9fda13b5042cd8052434bacbf4d294fad
Rudo2204 <rudo2204@gmail.com>
2021-06-09 20:25:19 +0700

    Remove data_dir related fns

--- src/librpl/util.rs
+++ src/librpl/util.rs
@@ -1,25 +1,6 @@
 use directories::ProjectDirs;
 use log::debug;
-use std::path::{Path, PathBuf};
-
-pub fn get_data_dir(
-    qualifier: &str,
-    organization: &str,
-    application: &str,
-) -> anyhow::Result<PathBuf> {
-    let proj_dirs = ProjectDirs::from(&qualifier, &organization, &application)
-        .expect("Could not retrieve ProjectDirs, maybe you are using an unsupported OS");
-    Ok(proj_dirs.data_dir().to_path_buf())
-}
-
-pub fn create_data_dir(data_dir: &Path) -> anyhow::Result<()> {
-    if !data_dir.exists() {
-        debug!("Project data dir does not exist, creating them...");
-        std::fs::create_dir_all(data_dir)?;
-        debug!("Successfully created data dirs");
-    }
-    Ok(())
-}
+use std::path::PathBuf;
 
 pub fn get_conf_dir(
     qualifier: &str,
@@ -45,5 +26,7 @@ pub fn create_proj_conf(
         &application,
         conf_dir.display()
     );
+
+    std::fs::create_dir_all(conf_dir).expect("Could not create config dir");
     Ok(())
 }
