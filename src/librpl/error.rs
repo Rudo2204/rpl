2dd3f014d55ceea59323b44a63775639bba5fd86
Rudo2204 <rudo2204@gmail.com>
2021-06-19 18:11:59 +0700

    Clean up unused crates

--- src/librpl/error.rs
+++ src/librpl/error.rs
@@ -6,8 +6,6 @@ pub enum Error {
     ToStringError(#[from] reqwest::header::ToStrError),
     #[error("Serde json could not correctly deserialize: {0}")]
     SerdeJson(#[from] serde_json::Error),
-    #[error("Serde_urlencoded could not serialize the url: {0}")]
-    SerdeUrl(#[from] serde_urlencoded::ser::Error),
     #[error("Header value was malformed: {0}")]
     HeaderError(#[from] reqwest::header::InvalidHeaderValue),
     #[error("Header value was not correctly set")]
